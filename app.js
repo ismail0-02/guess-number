7d2b96b2f7ba5d5cfd581c2ecf22659bf7016067
ismailo02 <muhammadismailcupcake@gmail.com>
2025-10-23 21:48:56 +0500

    Play the game!

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-let secretNum = 5;
-let guessNum = prompt("Guess the number ranging from (1-10)")
-if (guessNum == secretNum){
-    alert("Bingo! correct answer")
-}
-else if (guessNum == ++secretNum    ){
-     alert("Close enough to the correct answer")
-}
-else if (guessNum == 4  ){
-     alert("Better guess but not correct try again!")
-}
+let guessNum =5
 
-else{
-     alert("Wrong Answer!")
+let userNum = +prompt("Guess the num")
+if(guessNum == userNum){
+    console.log("Bingo! Correct answer");   
 }
+ else if (guessNum == userNum -1 || guessNum == userNum +1 ){
+  console.log("One step close to the answer!!")
+}
+ else if (guessNum == userNum -2 || guessNum == userNum +2 ){
+  console.log("Thura sa dur ha!!")
+}
+ else if (guessNum == userNum -3 || guessNum == userNum +3 ){
+  console.log("Required more practice!!")
+}
+ else if (guessNum == userNum -4|| guessNum == userNum +4 ){
+  console.log("bht kamzor ho tum mazed practice ki zrort ha!!")
+}
+else{
+    console.log("tumse na ho payega ghr ja bhai !!!!!!");
+    
+}
+alert("Check the console to check weather your answer is right or wrong")
+
